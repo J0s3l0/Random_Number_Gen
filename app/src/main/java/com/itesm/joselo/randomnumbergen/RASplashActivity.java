3f35ed3043c0b7007db387d14732314fffc55379
Jose Luis <A01063645@itesm.mx>
2015-11-02 02:00:37 -0600

    +First Commit - SplashScreen added

---
+++ app/src/main/java/com/itesm/joselo/randomnumbergen/RASplashActivity.java
@@ -0,0 +1,34 @@
+package com.itesm.joselo.randomnumbergen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class RASplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(RASplashActivity.this, RAGenActivity.class);
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
